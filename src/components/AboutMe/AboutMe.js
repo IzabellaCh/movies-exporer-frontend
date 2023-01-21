46ae68f75adbc513f026438a89ad77f7695e44f7
Izabella Chebakova <chebakovaizabella@gmail.com>
2023-01-21 19:48:18 +0400

    Корреекция компонентов aboutme, portfolio

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import './AboutMe.css';
+import avatar from '../../images/avatar.jpg';
+import { Link } from 'react-router-dom';
 
 function AboutMe() {
   return (
-    <div>
-      <h>AboutMe</h>
+    <div className='about-me section'>
+      <div className='about-me__line section__line'></div>
+      <div className='about-me__info'>
+        <article className='about-me__article'>
+          <h2 className='about-me__title'>Виталий</h2>
+          <p className='about-me__subtitle'>Фронтенд-разработчик, 30 лет</p>
+          <p className='about-me__description'>Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена 
+и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+          <Link className='about-me__github link-opacity' to=''>Github</Link>
+        </article>
+        <img className='about-me__avatar' src={avatar} alt='Аватар.'></img>
+      </div>
     </div>
   )
 };
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import './Portfolio.css';
+import { Link } from 'react-router-dom';
 
 function Portfolio() {
   return (
-    <div>
-      <h>Portfolio</h>
+    <div className='portfolio section'>
+      <h2 className='portfolio__title'>Портфолио</h2>
+      <ul className='portfolio__list'>
+        <li className='portfolio__element'>
+          <Link to='' className='portfolio__link link-opacity'>
+            <p className='portfolio__link-name'>Статичный сайт</p>
+            <div className='portfolio__arrow'></div>
+          </Link>
+        </li>
+        <li className='portfolio__element'>
+          <Link to='' className='portfolio__link link-opacity'>
+            <p className='portfolio__link-name'>Адаптивный сайт</p>
+            <div className='portfolio__arrow'></div>
+          </Link>
+        </li>
+        <li className='portfolio__element'>
+          <Link to='' className='portfolio__link link-opacity'>
+            <p className='portfolio__link-name'>Одностраничное приложение</p>
+            <div className='portfolio__arrow'></div>
+          </Link>
+        </li>
+      </ul>
     </div>
   )
 };
