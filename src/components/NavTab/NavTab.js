59e7fb359c2cd4534013af1d7a5e63a02cc6aee8
Izabella Chebakova <chebakovaizabella@gmail.com>
2023-01-22 15:40:01 +0400

    Добавление навигации по странице при помощи HashLink

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './NavTab.css';
-import { Link } from 'react-router-dom';
-
+import { HashLink as Link } from 'react-router-hash-link';
 
 function NavTab() {
   return (
     <div className='nav-tab'>
       <nav className='nav-tab__menu'>
-        <Link to='' className='nav-tab__link link-opacity'>О проекте</Link>
-        <Link to='' className='nav-tab__link link-opacity'>Технологии</Link>
-        <Link to='' className='nav-tab__link link-opacity'>Студент</Link>
+        <Link to='/#about-project' className='nav-tab__link link-opacity'>О проекте</Link>
+        <Link to='/#techs' className='nav-tab__link link-opacity'>Технологии</Link>
+        <Link to='/#about-me' className='nav-tab__link link-opacity'>Студент</Link>
       </nav>
     </div>
   )
