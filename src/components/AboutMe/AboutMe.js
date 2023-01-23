e93946fb4260b97af86eebb53fd35c06e9fd87b1
Izabella Chebakova <chebakovaizabella@gmail.com>
2023-01-23 20:30:59 +0400

    Добавление адаптивной верстки для компонентов aboutme, portfolio

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -6,10 +6,11 @@ import { Link } from 'react-router-dom';
 function AboutMe() {
   return (
     <div className='about-me section' id="about-me">
+      <h2 className='about-me__title section__title'>Студент</h2>
       <div className='about-me__line section__line'></div>
       <div className='about-me__info'>
         <article className='about-me__article'>
-          <h2 className='about-me__title'>Виталий</h2>
+          <h2 className='about-me__name'>Виталий</h2>
           <p className='about-me__subtitle'>Фронтенд-разработчик, 30 лет</p>
           <p className='about-me__description'>Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена 
 и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
