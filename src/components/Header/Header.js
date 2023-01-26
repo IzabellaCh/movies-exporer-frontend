591a3fff88249666f419cb479067995659688752
Izabella Chebakova <chebakovaizabella@gmail.com>
2023-01-26 20:56:50 +0400

    Вынесение кнопки Аккаунт из header в navigation, корректировка класса в searchform

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import Navigation from '../Navigation/Navigation';
 import logo from '../../images/logo.svg';
@@ -6,6 +6,7 @@ import { useNavigate } from 'react-router-dom';
 
 function Header({ loggedIn }) {
   const navigate = useNavigate();
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
 
   const toMain = () => {
     navigate('/');
@@ -15,26 +16,23 @@ function Header({ loggedIn }) {
     navigate('/signin');
   };
 
-  const toProfile = () => {
-    navigate('/profile');
+  const openMenu = () => {
+    setMenuIsOpen(true);
   }
 
-  const openMenu = () => {
-
+  const closeMenu = () => {
+    setMenuIsOpen(false);
   }
 
   return (
     <header className={`header ${loggedIn? 'header_type_logged': 'header_type_unlogged'} `}>
       <img onClick={toMain} className='header__logo button-opacity' src={logo} alt='Логотип.' />
-      <Navigation loggedIn={loggedIn} />
+      <Navigation loggedIn={loggedIn} menuIsOpen={menuIsOpen} closeMenu={closeMenu} />
       {!loggedIn && (
-        <button onClick={toLogin} className='header__button-to-page header__button-to-page_type_login button-opacity'>Войти</button>
+        <button onClick={toLogin} className='header__button-to-login button-opacity'>Войти</button>
       )}
       {loggedIn && (
-        <>
-          <button onClick={toProfile} className='header__button-to-page header__button-to-page_type_profile button-opacity'>Аккаунт</button>
-          <button onClick={openMenu} className='header__button-menu button-opacity'></button>
-        </>
+        <button onClick={openMenu} className='header__button-menu button-opacity'></button>
       )}
     </header>
   )
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,40 +1,46 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import "./Navigation.css";
+import { useNavigate } from 'react-router-dom';
 
-function Navigation({ loggedIn }) {
+function Navigation({ loggedIn, menuIsOpen, closeMenu }) {
+    const navigate = useNavigate();
+  
+  const toProfile = () => {
+    navigate('/profile');
+  }
+
   return (
     <div className={`navigation ${loggedIn ? "navigation_type_logged" : "navigation_type_unlogged"}`}>
       {loggedIn && (
         <>
-          <NavLink
-            to="/movies"
-            className={({ isActive }) =>
-              `${
-                isActive ? "navigation__link_type_active" : ""
-              } navigation__link navigation__link_type_logged link-opacity`
-            }
-          >
-            Фильмы
-          </NavLink>
-          <NavLink
-            to="/saved"
-            className={({ isActive }) =>
-              `${
-                isActive ? "navigation__link_type_active" : ""
-              } navigation__link navigation__link_type_logged link-opacity`
-            }
-          >
-            Сохранённые&nbsp;фильмы
-          </NavLink>
+          <div className='navigation__inserted-menu'>
+            <nav className='navigation__links'>
+              <NavLink to="/movies" className={({ isActive }) => `${isActive ? "navigation__link_type_active" : ""} navigation__link navigation__link_type_logged link-opacity`}>
+                Фильмы
+              </NavLink>
+              <NavLink to="/saved" className={({ isActive }) => `${isActive ? "navigation__link_type_active" : ""} navigation__link navigation__link_type_logged link-opacity`}>
+                Сохранённые&nbsp;фильмы
+              </NavLink>
+            </nav>
+            <button onClick={toProfile} className='navigation__button-to-profile button-opacity'>Аккаунт</button>
+          </div>
+          <div className={`navigation__separate-menu ${menuIsOpen? 'navigation__separate-menu_type_open': '' }`}>
+            <nav className='navigation__menu-container'>
+              <NavLink to="/" className={({ isActive }) => `${isActive ? "navigation__link_type_active" : ""} navigation__link navigation__link_type_logged link-opacity`}>Главная</NavLink>
+              <NavLink to="/movies" className={({ isActive }) => `${isActive ? "navigation__link_type_active" : ""} navigation__link navigation__link_type_logged link-opacity`}>Фильмы</NavLink>
+              <NavLink to="/saved" className={({ isActive }) => `${isActive ? "navigation__link_type_active" : ""} navigation__link navigation__link_type_logged link-opacity`}>Сохранённые&nbsp;фильмы</NavLink>
+            </nav>
+            <button className='navigation__button-close-menu'></button>
+          </div>
         </>
       )}
       {!loggedIn && (
-        <>
+        <nav>
           <Link to="/singup" className="navigation__link link-opacity">
             Регистрация
           </Link>
-        </>
+        </nav>
       )}
     </div>
   );
