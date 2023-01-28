4c39f2bad4b093c095c7c897293d137095d20897
Izabella Chebakova <chebakovaizabella@gmail.com>
2023-01-28 15:00:49 +0400

    Верстка компонента Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import './Profile.css';
+import Header from '../Header/Header';
+import { Link } from 'react-router-dom';
 
 function Profile() {
+  const pageIsMain = false;
   return (
-    <div>
-      Profile
+    <div className='profile'>
+      <Header pageIsMain={pageIsMain} />
+      <div className='progile__container'>
+        <h2 className='profile__title'>Привет, Виталий!</h2>
+        <ul className='profile__info'>
+          <li className='profile__element'>
+            <p className='profile__element-name'>Имя</p>
+            <p className='profile__element-value'>Виталий</p>
+          </li>
+          <li className='profile__element'>
+            <p className='profile__element-name'>E-mail</p>
+            <p className='profile__element-value'>pochta@yandex.ru</p>
+          </li>
+        </ul>
+        <div className='profile__links'>
+          <Link className='profile__link' to=''>Редактировать</Link>
+          <Link className='profile__link profile__link_type_red' to=''>Выйти из аккаунта</Link>
+        </div>
+      </div>
     </div>
   )
 };
