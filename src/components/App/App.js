a473e36f078766898dedace8c5e928e574303d0a
Izabella Chebakova <chebakovaizabella@gmail.com>
2023-01-29 14:28:30 +0400

    Добавление вертски ошибки

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import './App.css';
-import { Route, Routes, Switch, withRouter } from 'react-router-dom';
-import Main from '../Main/Main';
-import Movies from '../Movies/Movies';
-import SavedMovies from '../SavedMovies/SavedMovies';
-import Profile from '../Profile/Profile';
-import Login from '../Login/Login';
-import Register from '../Register/Register';
+import React, { useState } from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Main from "../Main/Main";
+import Movies from "../Movies/Movies";
+import SavedMovies from "../SavedMovies/SavedMovies";
+import Profile from "../Profile/Profile";
+import Login from "../Login/Login";
+import Register from "../Register/Register";
 
+import Error from "../Error/Error";
 
 function App() {
+  const [isError, setIsError] = useState(false);
 
   return (
     <div className="page">
       <Routes>
-        <Route exact path='/' element={<Main />} />
-        <Route path='/movies' element={<Movies />} />
-        <Route path='/saved' element={<SavedMovies />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/signin' element={<Login />} />
-        <Route path='/singup' element={<Register />} />
+        <Route exact path="/" element={<Main />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/saved" element={<SavedMovies />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<Login />} />
+        <Route path="/singup" element={<Register />} />
+        <Route path="/error" element={<Error />} />
       </Routes>
+      <Error isError={isError} />
     </div>
   );
 }
@@ -28,4 +32,3 @@ function App() {
 export default App;
 
 // export default withRouter(App);
-
