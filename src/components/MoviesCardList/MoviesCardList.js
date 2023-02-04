1c90f301c967e14b0e4506d73da967e620d74da8
Izabella Chebakova <chebakovaizabella@gmail.com>
2023-02-04 10:39:07 +0400

    Добавление зависимости количества карточек от ширины экрана, добавление карточек по клику на кнопку еще

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./MoviesCardList.css";
 import MoviesCard from "../MoviesCard/MoviesCard";
 
 function MoviesCardList({ isSavedMovies, movies }) {
+  // количество видимых фильмов на странице
+  const [numberOfVisibleMovies, setNumberOfVisibleMovies] = useState(0);
+
+  const addMovies = () => {
+    if (window.innerWidth > 1272) {
+      setNumberOfVisibleMovies(numberOfVisibleMovies + 3);
+    } else {
+      setNumberOfVisibleMovies(numberOfVisibleMovies + 2);
+    }
+  };
+
+  // расчет первоначального количества видимых фильмов на странице
+  useEffect(() => {
+    if (window.innerWidth > 1272) {
+      setNumberOfVisibleMovies(12);
+    } else if (window.innerWidth > 631 && window.innerWidth < 1273) {
+      setNumberOfVisibleMovies(8);
+    } else {
+      setNumberOfVisibleMovies(5);
+    }
+  }, []);
+
+  useEffect(() => {});
+
   return (
     <section
       className={`${
@@ -11,12 +35,30 @@ function MoviesCardList({ isSavedMovies, movies }) {
       aria-label="Список фильмов"
     >
       <div className="movies-card-list__elements">
-        {movies.map((item) => (
-          <MoviesCard info={item} key={item.id} isSavedMovies={isSavedMovies} />
-        ))}
+        {!isSavedMovies &&
+          movies
+            .slice(0, numberOfVisibleMovies)
+            .map((item) => (
+              <MoviesCard
+                info={item}
+                key={item.id}
+                isSavedMovies={isSavedMovies}
+              />
+            ))}
+        {isSavedMovies &&
+          movies.map((item) => (
+            <MoviesCard
+              info={item}
+              key={item.id}
+              isSavedMovies={isSavedMovies}
+            />
+          ))}
       </div>
       {!isSavedMovies && (
-        <button className="movies-card-list__button-add-movies button-opacity">
+        <button
+          onClick={addMovies}
+          className="movies-card-list__button-add-movies button-opacity"
+        >
           Ещё
         </button>
       )}
